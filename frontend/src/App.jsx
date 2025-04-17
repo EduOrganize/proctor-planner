050240ea93a246c1151960aab3ddca63a4b394a0
tymey <tyler.meyer.a@gmail.com>
2025-04-17 12:17:13 -0500

    build: Implement React-Router for client side routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,15 @@
-import React, {useEffect, useState } from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router';
+
+import Home from './views/Home';
 
 function App() {
-  const [message, setMessage] = useState('Loading...');
-
-  // Call the backend API endpoint to fetch the "Hello World" message
-  const getHello = () => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(data => setMessage(data))
-      .catch(err => {
-        console.error("Failed to getHello: ", err);
-        setMessage("Error loading message.");
-      });
-  };
-
-  useEffect(() => {
-    getHello();
-  }, []);
-
   return (
-    <div>
-      <h1>{message}</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
