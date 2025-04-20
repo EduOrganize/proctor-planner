a24425850978b0d453481374ef71e841e648fe00
TYLER MEYER <tylermeyer@macbook-pro.mynetworksettings.com>
2025-04-19 21:50:51 -0500

    Remove Java Spring-Boot backend

--- backend/src/main/java/com/eduorganize/proctor_planner/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eduorganize.proctor_planner.repository;
-
-import com.eduorganize.proctor_planner.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
