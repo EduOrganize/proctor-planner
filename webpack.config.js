a3a571e87f84323a503521cb6641f6faadca3f8f
TYLER MEYER <tylermeyer@macbook-pro.mynetworksettings.com>
2025-04-19 22:10:21 -0500

    move: Webpack updated and moved to root dir

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // Use 'production' for production builds
+  entry: './src/index.jsx', // Entry point of your React code
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/', // For SPA routing support
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Allow imports without specifying extensions
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    hot: true,                 // enable hot‑reload
+    historyApiFallback: true,  // for client‑side routing
+    proxy: [
+      // any request to /api/* will be forwarded to your Node server
+      {
+        context: ['/api'],
+        target: 'http://localhost:8080',
+        secure: false,
+        changeOrigin: true,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html' // Template HTML file
+    }),
+  ],
+};
