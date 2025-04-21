be3b5dab943e5b3153deacd0ff0d2ca16a118a61
TYLER MEYER <tylermeyer@TYLERs-MacBook-Pro.local>
2025-04-20 21:57:56 -0500

    build: Convert backend to TypeScript

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -1,5 +1,5 @@
-const path = require('path');
-const express = require('express');
+import path from 'path';
+import express, { Request, Response } from 'express';
 
 const DIST_DIR = path.resolve(__dirname, '..', '..', 'dist');
 
@@ -8,13 +8,13 @@ const app = express();
 app.use(express.static(DIST_DIR));
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/hello', (req: Request, res: Response) => {
   res.status(200);
   res.send({ message: "Hello from Node.js/Express Server!" });
 });
 
-app.get('/{*splat}', (req, res) => {
+app.get('/{*splat}', (req: Request, res: Response) => {
   res.sendFile('index.html', { root: DIST_DIR });
 });
 
-module.exports = app;
+export default app;
